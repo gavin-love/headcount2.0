0fc3a02612a31a1d92b91adf2a5d41dc8ff0f09e
gavin-love <1co.loco@oprotonmail.ch>
2018-06-25 15:41:52 -0600

    Pass test two of iteration0

--- src/helper.js
+++ src/helper.js
@@ -2,8 +2,20 @@ import React from "react";
 
 export default class DistrictRepository {
   constructor(props) {
-    this.stats = {};
+    this.stats = this.noDuplicates(props);
   }
+
+  noDuplicates = props => {
+    return props.reduce((allDistricts, school) => {
+      const upperCaseDistrict = school.Location.toUpperCase();
+      if (!allDistricts[upperCaseDistrict]) {
+        allDistricts[upperCaseDistrict] = {
+          Location: upperCaseDistrict
+        };
+      }
+      return allDistricts;
+    }, {});
+  };
 
   render() {
     return <h1>hello</h1>;
