70ca91792ac201422516eff72afffc794908fb85
Ryan Harrington <Ryanaharrington5@gmail.com>
2018-06-27 15:06:16 -0600

    Create displayDistricts method to set kinderData to state

---
+++ src/components/Card.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Card = (props) => {
+  
+  return (
+    <div>
+      <h1>{props.location}</h1>
+      <ul></ul>
+    </div>
+  )
+}
+
+export default Card;
---
+++ src/components/CardContainer.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Card from './Card';
+
+const CardContainer = (props) => {
+  // console.log(props)
+  // const allDistricts = props.map(district => {
+  //   return <Card location={district.Location} stats={district.stats}/>
+  return (
+    <h1>hi</h1>
+  )
+  }
+
+  // {allDistricts}
+
+
+export default CardContainer;
