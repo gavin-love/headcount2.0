c3146f6fed081f67a381487ee4619013b5039f8b
gavin-love <1co.loco@oprotonmail.ch>
2018-07-02 10:15:09 -0600

    Add propTypes to CardContianer

--- src/components/CardContainer.js
+++ src/components/CardContainer.js
@@ -1,17 +1,25 @@
-import React from 'react';
-import Card from './Card';
-import PropTypes from 'prop-types';
-import  '../styles/CardContainer.css'
+import React from "react";
+import Card from "./Card";
+import PropTypes from "prop-types";
+import "../styles/CardContainer.css";
 
-const CardContainer = ({districts, findAverages, selectedDistricts}) => {
+const CardContainer = ({ districts, findAverages }) => {
   const allDistricts = districts.map((district, index) => {
-    return <Card location={district.Location} stats={district.stats} key={index} selected={district.selected}  findAverages={findAverages} />
-  })
-  return (
-    <div className="card-container">
-    {allDistricts}
-    </div>
-  )
-}
+    return (
+      <Card
+        location={district.Location}
+        stats={district.stats}
+        key={index}
+        findAverages={findAverages}
+      />
+    );
+  });
+  return <div className="card-container">{allDistricts}</div>;
+};
+
+CardContainer.propTypes = {
+  districts: PropTypes.array.isRequired,
+  findAverages: PropTypes.func.isRequired
+};
 
 export default CardContainer;
